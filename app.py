4bd9e7f09a73e55b902536970d12f2e1867bbb5e
mohaned-abid <mouhaned.abid@supcom.tn>
2020-08-13 22:38:07 +0100

    Update app.py

--- app.py
+++ app.py
@@ -48,44 +48,31 @@ print('Model loaded. Start serving...')
 
 
 def model_predict(img_path):
-        # load the input image from disk, scale the pixel intensities to the
-        # range [0, 1], and then convert the image from the BGR to Lab color
-        # space
+        # load the input image from disk
         image = cv2.imread(img_path)
         scaled = image.astype("float32") / 255.0
         lab = cv2.cvtColor(scaled, cv2.COLOR_BGR2LAB)
 
-        # resize the Lab image to 224x224 (the dimensions the colorization
-        # network accepts), split channels, extract the 'L' channel, and then
-        # perform mean centering
+        # resize the Lab image to 224x224
         resized = cv2.resize(lab, (224, 224))
         L = cv2.split(resized)[0]
         L -= 50
 
-        # pass the L channel through the network which will *predict* the 'a'
-        # and 'b' channel values
+        # pass the L channel through the network
         'print("[INFO] colorizing image...")'
         net.setInput(cv2.dnn.blobFromImage(L))
         ab = net.forward()[0, :, :, :].transpose((1, 2, 0))
 
-        # resize the predicted 'ab' volume to the same dimensions as our
-        # input image
+        # resize the predicted 'ab' volume 
         ab = cv2.resize(ab, (image.shape[1], image.shape[0]))
 
-        # grab the 'L' channel from the *original* input image (not the
-        # resized one) and concatenate the original 'L' channel with the
-        # predicted 'ab' channels
+        # grab the 'L' channel from the *original* input images
         L = cv2.split(lab)[0]
         colorized = np.concatenate((L[:, :, np.newaxis], ab), axis=2)
 
-        # convert the output image from the Lab color space to RGB, then
-        # clip any values that fall outside the range [0, 1]
+        # convert the output image from the Lab color space to RGB
         colorized = cv2.cvtColor(colorized, cv2.COLOR_LAB2BGR)
         colorized = np.clip(colorized, 0, 1)
-
-        # the current colorized image is represented as a floating point
-        # data type in the range [0, 1] -- let's convert to an unsigned
-        # 8-bit integer representation in the range [0, 255]
         colorized = (255 * colorized).astype("uint8")
         return colorized
 
@@ -99,7 +86,6 @@ def upload():
         for filename in os.listdir('static/'):
             if filename.startswith('gg'): 
                 os.remove('static/' + filename)
-        # Save the file to ./uploads
         basepath = os.path.dirname(__file__)
         x="gguploaded"+str(time.time())+".jpg"
         file_path = os.path.join(
